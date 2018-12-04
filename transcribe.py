d823a8e0e2b6dc5934b7188b0a42357b008b01b0
Jordan Koelbl <jkoelbl@csumb.edu>
2018-12-03 18:56:07 -0800

    Added main functionality

---
+++ transcribe.py
@@ -0,0 +1,24 @@
+from auxil import get_encoding
+import sys
+
+def is_valid_message(raw, codes):
+    for i in range(len(raw)):
+        if raw[i] not in codes:
+            return False
+    return True
+
+def get_message(raw, codes):
+    msg = ''
+    for i in range(len(raw)):
+        msg += codes[raw[i]]
+    return msg
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        raw = sys.argv[1]
+        codes = get_encoding()
+        if is_valid_message(raw, codes):
+            msg = get_message(raw, codes)
+            print(msg)
+        else:
+            print('not a valid message')
