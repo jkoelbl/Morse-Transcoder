b80f70448c3a4d3b14c5ffc255c9fb5ebc6274f5
Jordan Koelbl <jkoelbl@csumb.edu>
2018-12-06 15:41:25 -0800

    changed filetypes of codes and signals to CSV; modularized auxil.py, transcribe.py functionalities; created translate.py

--- auxil.py
+++ auxil.py
@@ -1,18 +1,36 @@
 import csv
 
-def get_encoding():
-    codes, signals = {}, {}
-    with open('signals.txt', newline='') as sigs:
+CODES = 'codes.csv'
+SIGNALS = 'signals.csv'
+
+def load_signals():
+    signals = {}
+    with open(SIGNALS, newline='') as sigs:
         reader = csv.reader(sigs, delimiter=',', quotechar='\"')
         for row in reader:
             signals[row[0]] = row[1]
-    with open('codes.txt', newline='') as cds:
+    return signals
+
+def get_full_signal(code, signals):
+    temp = ''
+    for i in range(len(code)):
+        temp += signals[code[i]]
+    return temp
+
+def get_encoding():
+    codes = {}
+    signals = load_signals()
+    with open(CODES, newline='') as cds:
         reader = csv.reader(cds, delimiter=',', quotechar='\"')
         for row in reader:
-            codes[row[0]] = row[1]
-    for key in codes.keys():
-        temp = ''
-        for i in range(len(codes[key])):
-            temp += signals[codes[key][i]]
-        codes[key] = temp
+            codes[row[0]] = get_full_signal(row[1], signals)
+    return codes
+
+def get_decoding():
+    codes = {}
+    signals = load_signals()
+    with open(CODES, newline='') as cds:
+        reader = csv.reader(cds, delimiter=',', quotechar='\"')
+        for row in reader:
+            codes[get_full_signal(row[1], signals)] = row[0]
     return codes
--- translate.py
+++ translate.py
@@ -1,27 +1,20 @@
-from auxil import get_encoding
+from auxil import get_decoding
 import sys
 
 def is_valid_message(raw, codes):
-    for i in range(len(raw)):
-        if raw[i] not in codes:
-            return False
     return True
 
 def get_message(raw, codes):
-    msg = ''
-    for i in range(len(raw)):
-        msg += codes[raw[i]]
-    return msg
+    return raw
 
-def encode():
+def decode():
     raw = ''
     if len(sys.argv) > 1:   raw = sys.argv[1].lower()
     else:                   raw = input('Enter Message: ')
-    codes = get_encoding()
+    codes = get_decoding()
     if is_valid_message(raw, codes):    msg = get_message(raw, codes)
     else:                               msg = 'not a valid message'
     return msg
 
-
 if __name__ == '__main__':
-    print(encode())
+    print(decode())
